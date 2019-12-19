31b105f1d8acce6d9c961352b61db99c37aca465
davidgeorgebell <bell.dave93@gmail.com>
2019-12-19 08:51:50 +0800

    Dark mode created and saved to local storage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState(getInitialMode);
+
+  useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(theme));
+  }, [theme]);
+
+  function getInitialMode() {
+    const savedMode = JSON.parse(localStorage.getItem('dark'));
+    return savedMode || false;
+  }
+
   return (
-    <div className='App'>
+    <div className={theme ? 'dark-mode' : 'light-mode'}>
       <nav>
         <div className='toggle-container'>
-          <button>Toggle</button>
+          <button onClick={() => setTheme((prevMode) => !prevMode)}>
+            Toggle
+          </button>
         </div>
       </nav>
       <main>
-        <h1>Light Mode</h1>
+        <h1>{theme ? 'Dark Mode' : 'Light Mode'}</h1>
         <h2>Push button to change theme</h2>
       </main>
     </div>
